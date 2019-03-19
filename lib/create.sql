7eeb166be3a80fb6289268c4d96b35af184ecf85
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:00:52 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,13 +1,9 @@
 CREATE TABLE bears (
-  //columns here
+  id INTEGER PRIMARY KEY,
+  name	TEXT,
+  age	INTEGER,
+  gender	TEXT,
+  color	TEXT,
+  temperament	TEXT,
+  alive	BOOLEAN
 );
-
-
-
-id	integer
-name	text
-age	integer
-gender	text
-color	text
-temperament	text
-alive	boolean
