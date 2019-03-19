27e6116a03f1eeffbe4dd97160429bb71d76a2ce
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:44:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mister Chocolate", 3, "M", "brown", "sweet", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "female", "brown", "goofy", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "brown", "menacing", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "rust", "adventurous", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "goofy", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "brown", "languid", "T");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "M", "black", "ornery", "F");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mister Chocolate", 3, "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "female", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "brown", "menacing", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "rust", "adventurous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "brown", "languid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "M", "black", "ornery", 0);
