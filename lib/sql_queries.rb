50d43e2d5764ecf76abed2361086e832ab3effb9
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:38:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,29 +3,29 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT bears.name FROM bears ORDER BY bears.name;"
+  "SELECT bears.name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age FROM bears WHERE bears.alive = 'T' ORDER BY bears.age DESC;"
+  "SELECT bears.name, bears.age FROM bears WHERE bears.alive = 'T' ORDER BY age DESC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears WHERE color = "rust";"
+  "SELECT COUNT(color) FROM bears WHERE bears.color = "rust";"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears where temperament = "goofy";"
+  "SELECT COUNT(temperament) FROM bears where bears.temperament = "goofy";"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = NULL;"
+  "SELECT * FROM bears WHERE bears.name = NULL;"
 end
