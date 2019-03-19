b7e9d898ec48374cdbaa4c00201f2affed115575
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:45:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears WHERE bears.color = 'rust';"
+  "SELECT COUNT(color) FROM bears WHERE color = 'rust';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
