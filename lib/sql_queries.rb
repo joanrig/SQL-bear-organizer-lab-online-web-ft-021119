6ebfe80e5ee7f9b0dadddb7e016b938875d0df99
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:28:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'female'"
+  "SELECT name, age FROM bears WHERE gender = "female";"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT 
+  "SELECT COUNT color FROM bears WHERE color = "rust";"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT temperament FROM bears where temperament = "goofy";"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name = NULL;"
 end
