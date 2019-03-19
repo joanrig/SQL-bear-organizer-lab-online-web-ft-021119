b5bae7ed87d19f2c18a1a0fcb0027e8978f04255
Joan Lyness <joanrigdon@gmail.com>
2019-03-19 00:34:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mister Chocolate", 3, "male", "brown", "sweet", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "black", "rowdy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mister Chocolate", 3, "M", "brown", "sweet", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "rowdy", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "female", "brown", "goofy", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "male", "brown", "menacing", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "rust", "adventurous", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "female", "brown", "goofy", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "brown", "languid", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "male", "black", "ornery", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "brown", "menacing", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "rust", "adventurous", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "goofy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "brown", "languid", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "M", "black", "ornery", "false");
